4bb1aee23f8506dbe34e68d398561b00e5f718cc
jakeHaln <jakeHaln@protonmail.com>
2022-05-21 15:08:28 +0900

    Day2: update

---
+++ LinkedList/AddTwoNumbers.java
@@ -0,0 +1,23 @@
+public class AddTwoNumbers {
+
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    int total = 0;
+    ListNode result = new ListNode(0);
+    ListNode ptr = result, q = l1, r = l2;
+    while (r != null || q != null) {
+      if (r != null) {
+        total += r.val;
+        r = r.next;
+      }
+      if (q != null) {
+        total += q.val;
+        q = q.next;
+      }
+      ptr.next = new ListNode(total % 10);
+      total /= 10;
+      ptr = ptr.next;
+    }
+    if (total != 0) ptr.next = new ListNode(1);
+    return result.next;
+  }
+}
