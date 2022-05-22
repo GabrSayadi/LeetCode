85fe780a04a423de2ac66b78e84aa5c2be82d3d6
jakeHaln <jakeHaln@protonmail.com>
2022-05-22 22:16:39 +0900

    Day3: length of longest sub string

---
+++ String/LengthOfLongestSubstring.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class LengthOfLongestSubstring {
+
+  public int lengthOfLongestSubstring(String s) {
+    int[] str = new int[150];
+    Arrays.fill(str, -1);
+    int len = 0, left = 0;
+    for (int right = 0; right < s.length(); right++) {
+      left = Math.max(left, str[s.charAt(right)] + 1);
+      str[s.charAt(right)] = right;
+      len = Math.max(len, right - left + 1);
+    }
+    return len;
+  }
+}
